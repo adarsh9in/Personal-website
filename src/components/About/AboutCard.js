6caaf720f14a895fc00d7f936e2948a4c3f0f6c5
adarsh <mishraadarsh530@gmail.com>
2023-08-25 17:03:57 +0530

    Updated About file with new information

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,22 +9,19 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Adarsh Mishra  </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
-            <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
-            <br />
+            from <span className="purple"> Hyderabad, India.</span>
+            <br /> I am a final year student pursuing B.Tech (Computer science)
+            From NIIT University Rajasthan
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
+            <li className="about-activity">git status
+
+              <ImPointRight />I'm a competitive Dancer 
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight />Music
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
